0384941558b931b5c12db4eb24e7c48dd595a6ae
Jagruthi9 <60088124+Jagruthi9@users.noreply.github.com>
2020-04-29 09:53:12 +0530

    Update Vendor.java
    added api validation

--- VendorManagement/src/main/java/com/cts/entity/Vendor.java
+++ VendorManagement/src/main/java/com/cts/entity/Vendor.java
@@ -6,17 +6,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity(name="Vendors")
 public class Vendor {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	
 	private Long vendorId;
+	@NotNull(message=" first name cannot be null")
+	@Size(min=8,message="name must be 8 characters" )
 	private String name;
 	
 	private String address;
+	@Pattern(regexp="^((\\+){1}91){1}[1-9]{1}[0-9]{9}$",message="must +91 be added while giving number")
+
 	private String mobileNo;
+	@NotNull(message=" Email name cannot be null")
+	@Email
 	private String emailId;
+	
 	
 	public Vendor() {
 		super();
